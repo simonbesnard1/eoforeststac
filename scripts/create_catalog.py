314998dc798c30872cc92155458677cf0d8055f3
Simon <besnard@gfz-potsdam.de>
2026-01-08 16:02:49 +0100

    fix github prefix

--- scripts/create_catalog.py
+++ scripts/create_catalog.py
@@ -1,4 +1,5 @@
 from eoforeststac.catalog.root import build_root_catalog
 
-catalog = build_root_catalog(write=True)
+catalog = build_root_catalog(write=True, build_browser=True)
 print("Root catalog built at:", catalog.self_href)
+
