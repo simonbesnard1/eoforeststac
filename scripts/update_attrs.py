17d7d9fbec89d0a5f684b5dc691e502b24d2750d
Simon <besnard@gfz-potsdam.de>
2026-03-04 13:18:51 +0100

    update crs liu biomass metadata

--- scripts/update_attrs.py
+++ scripts/update_attrs.py
@@ -14,9 +14,6 @@ import xarray as xr
 AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
 
-
-path = "s3://dog.atlaseo-glm.eo-gridded-data/collections/POTAPOV_HEIGHT/POTAPOV_HEIGHT_v1.0.zarr"
-
 storage_options = {
     "client_kwargs": {"endpoint_url": "https://s3.gfz-potsdam.de"},
     "anon": False,
@@ -24,23 +21,47 @@ storage_options = {
     "secret": AWS_SECRET_ACCESS_KEY,
 }
 
-mapper = fsspec.get_mapper(path, **storage_options)
+
+paths = ["s3://dog.atlaseo-glm.eo-gridded-data/collections/JRC_GFC2020/JRC_GFC2020_v3.0.zarr",
+         "s3://dog.atlaseo-glm.eo-gridded-data/collections/JRC_TMF/JRC_TMF_v2024.zarr",
+         "s3://dog.atlaseo-glm.eo-gridded-data/collections/GAMI/GAMI_v2.0.zarr",
+         "s3://dog.atlaseo-glm.eo-gridded-data/collections/GAMI/GAMI_v2.1.zarr",
+         "s3://dog.atlaseo-glm.eo-gridded-data/collections/GAMI/GAMI_v3.0.zarr",
+         "s3://dog.atlaseo-glm.eo-gridded-data/collections/GAMI/GAMI_v3.1.zarr"
+         ]
+
+for path in paths:
+    mapper = fsspec.get_mapper(path, **storage_options)
+    zg = zarr.open_group(mapper, mode="r+")  # needs write permission
+    if "crs" in zg.attrs:
+        zg.attrs["spatial_ref"] = zg.attrs["crs"]
+        del zg.attrs["crs"]
+    
+    # 3) Reconsolidate so consolidated=True reads see the updated attrs
+    consolidate_metadata(mapper)
+    print("Consolidated metadata updated.")
+
+
 
 # 1) Read with xarray to know what are actual data variables
-ds = xr.open_zarr(
-    mapper, consolidated=False
-)  # no need for consolidated to inspect names
-data_vars = list(ds.data_vars)
-print(f"Found {len(data_vars)} data_vars")
+# ds = xr.open_zarr(
+#     mapper, consolidated=False
+# )  # no need for consolidated to inspect names
+# data_vars = list(ds.data_vars)
+# print(f"Found {len(data_vars)} data_vars")
 
 # 2) Patch only those arrays in the Zarr group
 zg = zarr.open_group(mapper, mode="r+")  # needs write permission
 
-for v in data_vars:
-    arr = zg[v]
-    arr.attrs["grid_mapping"] = "spatial_ref"
+# for v in data_vars:
+#     arr = zg[v]
+#     arr.attrs["grid_mapping"] = "spatial_ref"
 
+
+#zg.attrs["crs"] = "spatial_ref"
 
 # 3) Reconsolidate so consolidated=True reads see the updated attrs
-consolidate_metadata(mapper)
-print("Consolidated metadata updated.")
+#consolidate_metadata(mapper)
+#print("Consolidated metadata updated.")
+
+
